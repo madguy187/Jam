1010ecf07d2418a281217bed3ec588a28c2f64cb
madguy187 <nubbiehood@hotmail.com>
2025-07-19 16:56:41 +0900

    Nico (#10)
    * Added combat loop
    
    * Added all assets
    
    * Added unit position priority

--- Assets/Resources/Script/Global.cs
+++ Assets/Resources/Script/Global.cs
@@ -2,9 +2,12 @@ using UnityEngine;
 
 public static class Global {
     public const int GRID_SIZE = 3;
+    public static bool IS_DEBUG = true;
+
+    // Combat
     public const int PERCENTAGE_CONSTANT = 100;
     public const int RESISTANCE_PERCENTAGE_CONSTANT = 10;
-    public static bool IS_DEBUG = true;
+    public const int INVALID_INDEX = -1;
 
     // Simulation test for Darren&Tianyu to verify all kinds of complicated scenarios
     public static bool RUN_SIMULATION = false; 
