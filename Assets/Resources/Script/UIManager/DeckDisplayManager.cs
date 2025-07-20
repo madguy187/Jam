32bf358ed2845b81dd07fdb4b479b76059d1d94f
fikrulsmith <56424891+fikrulsmith@users.noreply.github.com>
2025-07-20 18:21:53 +0800

    add edit button

--- Assets/Resources/Script/UIManager/DeckDisplayManager.cs
+++ Assets/Resources/Script/UIManager/DeckDisplayManager.cs
@@ -24,13 +24,13 @@ public class DeckDisplayManager : MonoBehaviour
         var currentUnits = new List<UnitObject>();
         foreach (UnitObject unit in DeckManager.instance.GetDeckByType(eDeckType.PLAYER))
         {
-            if (unit != null)
+            if (unit != null) {
                 currentUnits.Add(unit);
+            }
         }
 
         // Only update if the deck has changed
-        if (!AreUnitListsEqual(lastUnits, currentUnits))
-        {
+        if (!AreUnitListsEqual(lastUnits, currentUnits)) {
             RefreshDeckDisplay(currentUnits);
             lastUnits = new List<UnitObject>(currentUnits);
         }
@@ -38,10 +38,9 @@ public class DeckDisplayManager : MonoBehaviour
 
     bool AreUnitListsEqual(List<UnitObject> a, List<UnitObject> b)
     {
-        if (a.Count != b.Count) return false;
-        for (int i = 0; i < a.Count; i++)
-        {
-            if (a[i] != b[i]) return false;
+        if (a.Count != b.Count) { return false; }
+        for (int i = 0; i < a.Count; i++) {
+            if (a[i] != b[i]) { return false; }
         }
         return true;
     }
@@ -49,15 +48,13 @@ public class DeckDisplayManager : MonoBehaviour
     void RefreshDeckDisplay(List<UnitObject> units)
     {
         // Clear old boxes
-        foreach (var box in deckBoxes)
-        {
+        foreach (var box in deckBoxes) {
             Destroy(box);
         }
         deckBoxes.Clear();
 
         // Add boxes for current units
-        foreach (UnitObject unit in units)
-        {
+        foreach (UnitObject unit in units) {
             GameObject box = Instantiate(deckBoxPrefab, deckPanel);
             SpriteRenderer unitSr = unit.GetComponent<SpriteRenderer>();
             box.GetComponent<Image>().sprite = unitSr.sprite;
