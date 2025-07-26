1ef07a95a5ac43dcd8855e8ff4c8b472cc6a14b0
madguy187 <nubbiehood@hotmail.com>
2025-07-26 12:24:38 +0800

    Added relic feature to the loading

--- Assets/Resources/ScriptableObject/RelicSO/_Base/RelicScriptableObject.cs
+++ Assets/Resources/ScriptableObject/RelicSO/_Base/RelicScriptableObject.cs
@@ -5,7 +5,14 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Scriptable Object/Unit/RelicScriptableObject")]
 public class RelicScriptableObject : ScriptableObject, IEnumerable<EffectScriptableObject> 
 {
+#if UNITY_EDITOR
+    public void SetRelicName(string relicName) { strRelicName = relicName; }
+    public void SetRelicSprite(Sprite sprite) { spriteRelic = sprite; }
+    public void SetEffectList(List<EffectScriptableObject> list) { listEffect = list; }
+#endif
+    
     [SerializeField] string strRelicName = "";
+    [SerializeField] Sprite spriteRelic = null;
     [SerializeField] List<EffectScriptableObject> listEffect;
 
     public string GetRelicName() {
